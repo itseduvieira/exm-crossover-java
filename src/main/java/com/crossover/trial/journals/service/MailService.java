f4bcedab46bbb91ffceeb94b7f6af632179cf413
Eduardo Vieira <eduardofelipevieira@gmail.com>
2017-08-02 03:00:56 -0300

    Construção de envio de emails

--- src/main/java/com/crossover/trial/journals/service/MailService.java
+++ src/main/java/com/crossover/trial/journals/service/MailService.java
@@ -2,7 +2,9 @@ package com.crossover.trial.journals.service;
 
 import com.crossover.trial.journals.model.Category;
 
-public interface MailService {	
+public interface MailService {
+	boolean sendNow();
+	
 	void sendOnPublish(Category category);
 	
 	void sendDaily();
--- src/main/java/com/crossover/trial/journals/service/MailServiceImpl.java
+++ src/main/java/com/crossover/trial/journals/service/MailServiceImpl.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 
 import com.crossover.trial.journals.model.Category;
 import com.crossover.trial.journals.model.Subscription;
+import com.crossover.trial.journals.model.User;
 import com.crossover.trial.journals.repository.SubscriptionRepository;
+import com.crossover.trial.journals.repository.UserRepository;
 
 @Service
 public class MailServiceImpl implements MailService {
@@ -19,9 +21,13 @@ public class MailServiceImpl implements MailService {
 	
 	@Autowired
 	private SubscriptionRepository subscriptionRepository;
+	
+	@Autowired
+	private UserService userService;
 
-	private boolean sendNow() {
-		log.debug("MAIL SENT");
+	@Override
+	public boolean sendNow() {
+		log.info("MAIL SENT");
 		
 		return true;
 	}
@@ -29,17 +35,22 @@ public class MailServiceImpl implements MailService {
 	@Override
 	public void sendOnPublish(Category category) {
 		List<Subscription> subscriptions = subscriptionRepository.findByCategory(category);
-		subscriptions.stream().forEach(s -> log.debug(s.getUser().getLoginName()));
-		
-		sendNow();
+		subscriptions.stream().forEach(s -> {
+			log.info(s.getUser().getLoginName());
+			
+			sendNow();
+		});
 	}
 
 	@Override
 	//@Scheduled(cron = "0 0 4 * * ?")
-	@Scheduled(cron = "*/25 * * * * ?")
-	//@Async
+	@Scheduled(cron = "0 * * * * ?")
 	public void sendDaily() {
-		sendNow();
+		List<User> users = userService.findAll();
+		users.stream().forEach(u -> { 
+			log.info(u.getLoginName());
+			
+			sendNow();
+		});
 	}
-
 }
