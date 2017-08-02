cb0300885ad5ad0943daa9743263e465fe9b9545
Eduardo Vieira <eduardofelipevieira@gmail.com>
2017-08-02 02:58:42 -0300

    Adição de campos de email no usuário

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('publisher1', '$2a$10$RC1.YFSL8o2vCIg6kB3fjuBB639BdjY6AD8rIw9oyWJLws/1Q6Lku', TRUE, 'PUBLISHER');
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('publisher2', '$2a$10$MUahUza86ErCxtsgpmMBDeR5VtoGHioRdl03/jQmkM/sk6L.Eg28e', TRUE, 'PUBLISHER');
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('user1', '$2a$10$WcgRF8VQ8DKt4h4Hz9pWv.6MXnIRmcPr0j9jqsseprsBwTD4w8WSm', TRUE, 'USER');
-INSERT INTO user(login_name, pwd, enabled, role) VALUES ('user2', '$2a$10$Q5bxyPXhHXFc1fRUMCRWR.GbgsXx9aGZdoEoEAz2JFEfckdyUKfOi', TRUE, 'USER');
+INSERT INTO user(login_name, pwd, enabled, role, email) VALUES ('publisher1', '$2a$10$RC1.YFSL8o2vCIg6kB3fjuBB639BdjY6AD8rIw9oyWJLws/1Q6Lku', TRUE, 'PUBLISHER', 'publisher1@foo.bar');
+INSERT INTO user(login_name, pwd, enabled, role, email) VALUES ('publisher2', '$2a$10$MUahUza86ErCxtsgpmMBDeR5VtoGHioRdl03/jQmkM/sk6L.Eg28e', TRUE, 'PUBLISHER', 'publisher2@foo.bar');
+INSERT INTO user(login_name, pwd, enabled, role, email) VALUES ('user1', '$2a$10$WcgRF8VQ8DKt4h4Hz9pWv.6MXnIRmcPr0j9jqsseprsBwTD4w8WSm', TRUE, 'USER', 'user1@foo.bar');
+INSERT INTO user(login_name, pwd, enabled, role, email) VALUES ('user2', '$2a$10$Q5bxyPXhHXFc1fRUMCRWR.GbgsXx9aGZdoEoEAz2JFEfckdyUKfOi', TRUE, 'USER', 'user2@foo.bar');
 
 INSERT INTO publisher(user_id, name) VALUES (1, 'Test Publisher1 ');
 INSERT INTO publisher(user_id, name) VALUES (2, 'Test Publisher 2');
