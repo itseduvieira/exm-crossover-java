4de353d0ecdca91bb80357a2fa0f7058634373a9
Eduardo Vieira <eduardofelipevieira@gmail.com>
2017-08-02 00:17:25 -0300

    Adição de anotações para habilitar recursos

--- src/main/java/com/crossover/trial/journals/Application.java
+++ src/main/java/com/crossover/trial/journals/Application.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ComponentScan
 @EnableAutoConfiguration
+@EnableScheduling
+@EnableAsync
 public class Application {
 
 	public static final String ROOT;
